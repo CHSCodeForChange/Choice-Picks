c16c3dea2f01207d39318eb0bf08935374666ee7
Guna <gunavikasavula@gmail.com>
2021-02-23 18:49:30 -0500

    swicthed the librarian and nominations page bug

--- src/base/librarian.js
+++ src/base/librarian.js
@@ -1,9 +1,48 @@
-function Library() {
-    return (
-        <div>
-            <p>This is the librarian only access page.</p>
-        </div>
-    );
-}
+import React, {Component} from 'react';
+import '../index.css'
 
+class Library extends Component{
+    state = {
+        users: [
+            {username: "Guna", password: "asdf"},
+        ],
+    }
+
+    render() {
+        return (
+            <div>
+                <div id = "login">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                                Username: 
+                                <input type="text" id = "name" />
+                        </label>
+                        <br />
+                        <label>
+                                Password: 
+                                <input type="text" id = "password" />
+                        </label>
+                        <br />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+                <div id = "loginContent">
+                    <p>Inside</p>
+                </div>
+            </div>
+        );
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let username = document.getElementById("name").value;
+        let password = document.getElementById("password").value;
+        // let ru = this.state.users.username;
+        // if (username == this.state.users.get(0).username && password == this.state.users.get(0).password) {
+        if (username == "Guna" && password == "asdf") {
+            document.getElementById("login").style.display = "none";
+            document.getElementById("loginContent").style.display = "block";
+        }
+    }
+}
 export default Library;
