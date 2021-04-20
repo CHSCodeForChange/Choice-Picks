31791adc74b623c9682a7ea4922d77fb8a46ba12
Nick Wu <nickwu2005@icloud.com>
2021-04-20 18:18:41 -0400

    Carousel Dots added
    You can now shuffle between books on carousel using a line of dots below.

--- Client/src/base/choicePicks.js
+++ Client/src/base/choicePicks.js
@@ -1,34 +1,49 @@
 import React, {Component} from 'react';
-import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
+import Carousel, { slidesToShowPlugin, Dots } from '@brainhubeu/react-carousel';
 import '../../node_modules/@brainhubeu/react-carousel/lib/style.css';
 import '../index.css';
 import Book from './book';
-
+import ClickableView from './clickableView';
+import Collapsible from 'react-collapsible';
+ 
 class ChoicePicks extends Component {
-    render() {
-        return (
-            <div>
-                <Carousel
-                    plugins={[
-                        'infinite',
-                        'arrows',
-                        {
-                            resolve: slidesToShowPlugin,
-                            options: {
-                                numberOfSlides: 3
-                            }
-                        },
-                    ]}
-                >
-                    <Book name = 'Harry Potter' path = "https://images-na.ssl-images-amazon.com/images/I/81iqZ2HHD-L.jpg"/>
-                    <Book name = 'Hunger Games' path = "https://images-na.ssl-images-amazon.com/images/I/61JfGcL2ljL.jpg"/>
-                    <Book name = 'How to Code' path = "https://images-na.ssl-images-amazon.com/images/I/81mT0cKE0oL.jpg"/>
-                    <Book name = 'Scythe' path = "https://images-na.ssl-images-amazon.com/images/I/61dMoTN7t1L.jpg"/>
-                    <Book name = 'Dairy of a Wimpy Kid' path = "https://prodimage.images-bn.com/pimages/9781419741869_p0_v1_s550x406.jpg"/>
-                </Carousel>
-            </div>
-        );
-    }
+ 
+   constructor(props) {
+       super(props);
+       this.state = {
+         value: 0,
+       };
+     }
+       onChange = value => this.setState({ value });
+  
+   render() {
+       return (
+           <div>
+               <Carousel
+               value={this.state.value}
+               onChange={this.onChange}
+ 
+                   plugins={[
+                       'infinite',
+                       'arrows',
+                       {
+                           resolve: slidesToShowPlugin,
+                           options: {
+                               numberOfSlides: 3
+                           }
+                       },
+                   ]}
+               >
+                   <Book name = 'Harry Potter' path = "https://images-na.ssl-images-amazon.com/images/I/81iqZ2HHD-L.jpg"/>
+                   <Book name = 'Hunger Games' path = "https://images-na.ssl-images-amazon.com/images/I/61JfGcL2ljL.jpg"/>
+                   <Book name = 'How to Code' path = "https://images-na.ssl-images-amazon.com/images/I/81mT0cKE0oL.jpg"/>
+                   <Book name = 'Scythe' path = "https://images-na.ssl-images-amazon.com/images/I/61dMoTN7t1L.jpg"/>
+                   <Book name = 'Dairy of a Wimpy Kid' path = "https://prodimage.images-bn.com/pimages/9781419741869_p0_v1_s550x406.jpg"/>
+               </Carousel>
+               <Dots className="dots" value={this.state.value} onChange={this.onChange} number={8}/>
+           </div>
+       );
+   }
 }
-
 export default ChoicePicks;
+
